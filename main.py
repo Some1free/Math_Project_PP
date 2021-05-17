5c5449030bb3e97111f0ed4a787b185e0a1914a5
Some1free <matewol056@student.polsl.pl>
2021-05-17 13:25:59 +0200

    zapis_dla_bezp

--- main.py
+++ main.py
@@ -1,27 +1,53 @@
+INPUT_FILE = "config.cfg"
+OUTPUT_FILE = "results.txt"
+SHOW_CALC = False
+HELP_TEXT = "help.txt"
+
 CFG_SEPARATOR_1 = "="
 CFG_SEPARATOR_2 = ","
 
+CFG_TITLE_1 = "DL"
+CFG_TITLE_2 = "SILA"
+CFG_TITLE_3 = "MOMENT"
+CFG_TITLE_4 = "OB" #co z polskimi znakami i jeśli mam OBCIĄŻENIE _CIĄGŁE, OB CIAGŁE, OBCIĄŻENIE_C
 
+from getch import pause_exit
+import getopt, sysconfig, sys
+
+def help(help_file):
+    try:
+        file = open(help_file)
+        lines = file.readlines()
+        for l in range(len(lines)):    
+            print( lines[l])
+        ##pause_exit()
+    except:
+        print("No help file has been found")
+        ##pause_exit()
 
 def read_config(file_name):  
-    file = open(file_name)
-    lines = file.readlines()
+    try:
+        file = open(file_name)
+        lines = file.readlines()
 
-    dic = {}
+        dic = {}
 
-    for line in lines:
-        line = line.upper()
-        data = line.strip().replace(" ","").split(CFG_SEPARATOR_1)
-        name = data[0]
-        data = data[1].split(CFG_SEPARATOR_2)
-        
-        data = [float(x) for x in data]
 
-        dic[name] = data
-    
-    print("dic \n", dic)
+        for line in lines:
+            line = line.upper()
+            data = line.strip().replace(" ","").split(CFG_SEPARATOR_1)
+            name = data[0]
+            data = data[1].split(CFG_SEPARATOR_2)
+            
+            data = [float(x) for x in data]
 
-    return dic
+            dic[name] = data
+            return dic
+    except:
+        print("Błąd wczytywania pliku", file_name)
+        #pause_exit()
+
+   
 
 def get_value_from_config(cfg, name):
     name = name.upper()
@@ -29,7 +55,7 @@ def get_value_from_config(cfg, name):
     for n in cfg:
         if name in n:
             results.append(cfg[n])
-    return results 
+    return results
 
 def total_y_force(forces, loads):
     total = 0
@@ -58,32 +84,76 @@ def count_rb(torque, length):
 def count_ra(y_force, Rb):
     return y_force - Rb 
 
+def write_results_to_file(filename, ra, rb):
+    try:
+        f = open(filename, "w")
+        lines = ["Reakcje\n Ra: ", str(ra), "\n Rb: ", str(rb)]
+        f.writelines(lines)
+        print("Pomyślnie zapisano wyniki w pliku ", filename)
+    except:
+        print("Wystąpił problem z zapisaniem pliku")
+        #pause_exit()
+
+def calc(input, total_length, force_table, continuous_load_table, torque_table, sum1, sum2):
+    try:
+        f = open(filename, "a")
+        lines = ["input", input, "długość belki\n", str(total_length),"siły\n", force_table, "obciążenia\n", continuous_load_table,
+                "momenty\n", torque_table, "suma sił i obciążeń\n", str(sum1), "całkowity moment\n", str(sum2)]
+        f.writelines("\n", lines)
+        print("Pomyślnie zapisano wyniki w pliku ", filename)
+    except:
+        print("Wystąpił problem z zapisaniem pliku")
+        #pause_exit()   
+
+def command_menager(argv):
+    try:
+        options, args = getopt.getopt(sys.argv[1:], "hi:o:c", ["help", "input=", "output="])
+
+        for opt, arg in options:
+            if opt in ('-h', '--help'):
+                global HELP_TEXT
+                help(HELP_TEXT)
+                # #pause_exit()
+            if opt in ('-o', '--output'):
+                global OUTPUT_FILE
+                OUTPUT_FILE = arg
+            elif opt in ('-i', '--input'):
+                global INPUT_FILE
+                INPUT_FILE = arg
+            elif opt == '-c':
+                global SHOW_CALC
+                SHOW_CALC = True
+    except getopt.GetoptError:
+        print("Nie rozpoznano poprawnie arguemntów. Użyj -h aby uzyskać pomoc")
+        #pause_exit()
+
 def main():
-    file_name = ".\config.txt"
-
-    input = read_config(file_name)
-
-    total_length = get_value_from_config(input, "DL")
-    force_table = get_value_from_config(input, "SILA")
-    continuous_load_table = get_value_from_config(input, "OB") #co z polskimi znakami i jeśli mam OBCIĄŻENIE _CIĄGŁE, OB CIAGŁE, OBCIĄŻENIE_C
-    torque_table = get_value_from_config(input, "MOMENT")
-
-    print("długość belki\n", total_length)
-    print("siły\n",force_table)
-    print("obciążenia\n", continuous_load_table)
-    print("momenty\n", torque_table)
-
+    input = read_config(INPUT_FILE)
+    
+    total_length = get_value_from_config(input, CFG_TITLE_1)
+    force_table = get_value_from_config(input, CFG_TITLE_2)
+    continuous_load_table = get_value_from_config(input, CFG_TITLE_4) 
+    torque_table = get_value_from_config(input, CFG_TITLE_3)
+    
     sum1 = total_y_force(force_table, continuous_load_table)
-    print("suma sił i obciążeń\n", sum1)
-
     sum2 = total_tourge(force_table,continuous_load_table,torque_table)
-    print("całkowity moment\n", sum2)
-
     rb = count_rb(sum2, total_length)
     ra = count_ra(sum1, rb)
-    print("Reakcje\n Ra:", ra, "\n Rb:", rb)
+
+    write_results_to_file(OUTPUT_FILE, ra, rb)
+    if SHOW_CALC:
+        calc()
+
+if __name__ == "__main__":
+   main()  
+
+# print("input \n", input)
+# print("długość belki\n", total_length)
+# print("siły\n",force_table)
+# print("obciążenia\n", continuous_load_table)
+# print("momenty\n", torque_table)
+# print("suma sił i obciążeń\n", sum1)
+# print("całkowity moment\n", sum2)
 
 
-
-main()    
 
